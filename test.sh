e19616425084581585f05e70683ad5cdf1f04836
barkinrl <barkinoral.bo@gmail.com>
2025-05-04 06:03:14 +0300

    add: test run script
    Signed-off-by: barkinrl <barkinoral.bo@gmail.com>

--- test.sh
+++ test.sh
@@ -2,5 +2,8 @@
 
 set -e
 
-echo "Running unit tests..."
+echo "Running unit tests (go test)..."
 go test -tags=test ./internal/...
+
+echo "Running Ginkgo tests..."
+ginkgo -tags=test -r ./internal/...
