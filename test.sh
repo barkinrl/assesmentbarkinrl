b392f179a61a0ee49a955bb6ff09ddb6e34007a7
barkinrl <barkinoral.bo@gmail.com>
2025-05-05 17:46:05 +0300

    fix:test files
    Signed-off-by: barkinrl <barkinoral.bo@gmail.com>

--- test.sh
+++ test.sh
@@ -2,6 +2,20 @@
 
 set -e
 
+echo "🔧 Preparing test environment..."
+
+mkdir -p internal/static
+
+# static.go dosyasını üret
+cat > internal/static/static.go <<EOF
+package static
+
+import "embed"
+
+//go:embed *
+var Static embed.FS
+EOF
+
 echo "Running unit tests (go test)..."
 go test -tags=test ./internal/...
 
